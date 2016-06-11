0bdc87e81505c28c523d0d54d2b918e8c4c3f946
alon_ss <alon.sarshalom@gmail.com>
2016-06-11 13:58:37 +0300

    add @value support

--- src/main/java/com/alon/server/service/DaoServiceImpl.java
+++ src/main/java/com/alon/server/service/DaoServiceImpl.java
@@ -1,9 +1,14 @@
 package com.alon.server.service;
 
 import com.alon.server.entity.Message;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.annotation.Async;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
+
+
 
 import javax.annotation.PostConstruct;
 import java.sql.*;
@@ -13,25 +18,42 @@ import java.sql.*;
  */
 
 @Service
+@PropertySource("classpath:application.properties")
 public class DaoServiceImpl implements DaoService {
 
-    // TODO need to change it to a service
+    @Value("${db.username}")
+    private String DB_USER_NAME;
 
-    private final static String DB_NAME = "CHATS";
-    private final static String TABLE_NAME = "CHAT";
+    @Value("${db.password}")
+    private String DB_PASSWORD;
+
+    @Value("${db.connectionURL}")
+    private String JDBC_PLAIN_URL;
+
+    @Value("${db.name}")
+    private String DB_NAME;
+
+    @Value("${db.table.name}")
+    private String TABLE_NAME;
 
     private final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private final static String SERVER_TIMEZONE = "&serverTimezone=UTC";
     private final static String USE_SSL = "&useSSL=false";
     private final static String JDBC_PARAM = "?autoReconnect=true" + USE_SSL + SERVER_TIMEZONE;
 
-    private final static String JDBC_PLAIN = "jdbc:mysql://localhost";
-    private final static String DB_URL = JDBC_PLAIN + JDBC_PARAM;
-    private final static String DB_TABLE_URL = JDBC_PLAIN + "/" + DB_NAME + JDBC_PARAM;;
-    private final static String DB_PASSWORD = "1234";
+    private String dbUrl;
+    private String dbTableUrl;
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertyConfigInDev() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
 
     @PostConstruct
     private void init(){
+        dbUrl = JDBC_PLAIN_URL + JDBC_PARAM;
+        dbTableUrl = JDBC_PLAIN_URL + "/" + DB_NAME + JDBC_PARAM;
+
         testDbConnection();
     }
 
@@ -65,7 +87,7 @@ public class DaoServiceImpl implements DaoService {
 
     }
 
-    private static void testDbConnection() {
+    private void testDbConnection() {
 
         System.out.println("-------- MySQL JDBC Connection Testing ------------");
 
@@ -93,10 +115,10 @@ public class DaoServiceImpl implements DaoService {
         closeConnection(connection);
     }
 
-    private static Connection connectToDb() {
+    private Connection connectToDb() {
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection(DB_TABLE_URL,"root", DB_PASSWORD);
+            connection = DriverManager.getConnection(dbTableUrl, DB_USER_NAME, DB_PASSWORD);
         } catch (SQLException e) {
             System.out.println("Connection Failed! Check output console");
             createDb();
@@ -104,10 +126,10 @@ public class DaoServiceImpl implements DaoService {
         return connection;
     }
 
-    private static void createDb(){
+    private void createDb(){
         Connection dbConnection = null;
         try {
-            dbConnection = DriverManager.getConnection(DB_URL,"root", DB_PASSWORD);
+            dbConnection = DriverManager.getConnection(dbUrl, DB_USER_NAME, DB_PASSWORD);
             Statement dbStmt = dbConnection.createStatement();
             String createDbSql = "CREATE DATABASE "+ DB_NAME + " DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci";
             dbStmt.executeUpdate(createDbSql);
@@ -125,10 +147,10 @@ public class DaoServiceImpl implements DaoService {
         }
     }
 
-    private static void createTable(){
+    private void createTable(){
         Connection connection = null;
         try {
-            connection = DriverManager.getConnection(DB_TABLE_URL,"root", DB_PASSWORD);
+            connection = DriverManager.getConnection(dbTableUrl, DB_USER_NAME, DB_PASSWORD);
             Statement stmt = connection.createStatement();
             String sql = "CREATE TABLE " + TABLE_NAME +
                     "(id VARCHAR(45) not NULL, " +
