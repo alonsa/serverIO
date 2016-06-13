83736d40f15c9e6709c8c0163e200a79625096ee
alon_ss <alon.sarshalom@gmail.com>
2016-06-13 09:00:11 +0300

    refactor

--- src/main/java/com/alon/server/consts/Consts.java
+++ src/main/java/com/alon/server/consts/Consts.java
@@ -6,15 +6,16 @@ package com.alon.server.consts;
 public  class Consts {
 
 
-    // DB
-    public final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     private final static String SERVER_TIMEZONE = "&serverTimezone=UTC";
     private final static String USE_SSL = "&useSSL=false";
+
+    // DB
+    public final static String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
     public final static String JDBC_PARAM = "?autoReconnect=true" + USE_SSL + SERVER_TIMEZONE;
 
-//    Web socket
+    // Web socket
     public final static String GOODBYE = "Goodbye";
 
-//    User
+    // User
     public final static String LOBBY = "Lobby";
 }
